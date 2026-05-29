5e41c95e5c3274d2c8c648e83b2c239464254930
Blandine Jarysta <blandine.jarysta@gmail.com>
2026-05-29 16:10:01 +0200

    remove unnecessary padding from main layout

--- ui/app/components/MainLayout.tsx
+++ ui/app/components/MainLayout.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import { Box, styled } from '@mui/material';
-import SideMenu from './SideMenu';
+import React from "react";
+import { Box, styled } from "@mui/material";
+import SideMenu from "./SideMenu";
 
 const LayoutRoot = styled(Box)({
-  display: 'flex',
-  minHeight: '100vh',
-  width: '100%',
-  backgroundColor: '#fff',
+  display: "flex",
+  minHeight: "100vh",
+  width: "100%",
+  backgroundColor: "#fff",
 });
 
 const ContentWrapper = styled(Box)({
   flexGrow: 1,
-  display: 'flex',
-  flexDirection: 'column',
+  display: "flex",
+  flexDirection: "column",
   minWidth: 0,
-  padding: '24px',
 });
 
 interface MainLayoutProps {
@@ -25,9 +24,7 @@ export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
     <LayoutRoot>
       <SideMenu />
-      <ContentWrapper>
-        {children}
-      </ContentWrapper>
+      <ContentWrapper>{children}</ContentWrapper>
     </LayoutRoot>
   );
 };
