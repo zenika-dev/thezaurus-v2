ebde1e31179eef38f45bbbb687cc527b6338ae88
Blandine Jarysta <blandine.jarysta@gmail.com>
2026-05-29 16:09:16 +0200

    function in utils

--- ui/app/lib/utils.ts
+++ ui/app/lib/utils.ts
@@ -4,3 +4,8 @@ import { type ClassValue, clsx } from "clsx";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function isValidUrl(url: string) {
+  if (!url) return false;
+  return url.startsWith('http://') || url.startsWith('https://');
+};
