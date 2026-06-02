38a67afdb05797b000e336b3805db953d2a486fa
Blandine Jarysta <blandine.jarysta@gmail.com>
2026-06-02 10:58:25 +0200

    backend changes to blog post model

--- api/src/main/java/com/zenika/thezaurus/model/BlogPost.java
+++ api/src/main/java/com/zenika/thezaurus/model/BlogPost.java
@@ -1,17 +1,19 @@
 package com.zenika.thezaurus.model;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public class BlogPost {
     private String id;
     private String title;
     private List<String> writers;
-    private LocalDateTime creationDate;
-    private LocalDateTime publicationDate;
+    private String creationDate;
+    private String publicationDate;
     private String link;
+    private String status;
+    private List<String> tags;
 
-    public BlogPost() {}
+    public BlogPost() {
+    }
 
     public BlogPost(String id, String title, String link) {
         this.id = id;
@@ -19,21 +21,67 @@ public class BlogPost {
         this.link = link;
     }
 
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
+    public String getId() {
+        return id;
+    }
 
-    public List<String> getWriters() { return writers; }
-    public void setWriters(List<String> writers) { this.writers = writers; }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-    public LocalDateTime getCreationDate() { return creationDate; }
-    public void setCreationDate(LocalDateTime creationDate) { this.creationDate = creationDate; }
+    public String getTitle() {
+        return title;
+    }
 
-    public LocalDateTime getPublicationDate() { return publicationDate; }
-    public void setPublicationDate(LocalDateTime publicationDate) { this.publicationDate = publicationDate; }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-    public String getLink() { return link; }
-    public void setLink(String link) { this.link = link; }
+    public List<String> getWriters() {
+        return writers;
+    }
+
+    public void setWriters(List<String> writers) {
+        this.writers = writers;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(String publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
 }
