7ab47f276c709b3059eff5e4093c21943383796b
Blandine Jarysta <blandine.jarysta@gmail.com>
2026-06-15 15:05:16 +0200

    import theme provider

--- front/shared/lib/index.ts
+++ front/shared/lib/index.ts
@@ -1,3 +1,4 @@
 export { isValidUrl } from "./utils";
 export { default as theme } from "./theme";
 export { getQueryClient } from "./query-client";
+export { MuiThemeProvider } from "./MuiThemeProvider";
