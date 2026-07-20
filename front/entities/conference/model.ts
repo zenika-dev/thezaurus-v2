b1028ef20f5826b0850fc4181a3a35deefb918b5
Blandine Jarysta <blandine.jarysta@gmail.com>
2026-07-20 14:17:28 +0200

    model/schema for front

--- front/entities/conference/model.ts
+++ front/entities/conference/model.ts
@@ -1,5 +1,14 @@
 export type ConferenceCFPStatus = "Open" | "Closed" | "None";
 
+export type ConferenceType =
+  | "Marketing / business"
+  | "Technique stratégique"
+  | "Technique généraliste"
+  | "Technique"
+  | "Hors scope";
+
+export type ConferenceReach = "Locale" | "Régionale" | "Nationale";
+
 export type ConferenceDate =
     | { type: "single"; date: string }
     | { type: "range"; start: string; end: string }
@@ -17,14 +26,24 @@ export interface ConferenceData {
     title: string;
     location: ConferenceLocation;
     date: ConferenceDate;
-    cfpLink: string;
+    cfpLink?: string;
     cfpClosingDate?: string;
     cfpStatus: ConferenceCFPStatus;
     submittedTalksAmount: number;
+    type: ConferenceType;
+    reach: ConferenceReach;
 }
 
 export const conferenceCFPStatusConfig: Record<ConferenceCFPStatus, { text: string; bg: string; darkText: string; darkBg: string }> = {
   Open: { text: "#245E12", bg: "#DCFCE7", darkText: "#47FFB4", darkBg: "#0D542B" },
   Closed: { text: "#000000", bg: "#F7F7F7", darkText: "#FFFFFF", darkBg: "#5E5E5E" },
   None: { text: "#888888", bg: "transparent", darkText: "#888888", darkBg: "transparent" },
+};
+
+export const conferenceTypeConfig: Record<ConferenceType, { text: string; bg: string; darkText: string; darkBg: string }> = {
+  "Marketing / business": { text: "#000000", bg: "#F7F7F7", darkText: "#FFFFFF", darkBg: "#5E5E5E" },
+  "Technique stratégique": { text: "#9A0530", bg: "#FFEDD4", darkText: "#FFDD58", darkBg: "#7E2A0C" },
+  "Technique généraliste": { text: "#0132D1", bg: "#DBEAFE", darkText: "#94E5FF", darkBg: "#1C398E" },
+  "Technique": { text: "#245E12", bg: "#DCFCE7", darkText: "#47FFB4", darkBg: "#0D542B" },
+  "Hors scope": { text: "#681AC1", bg: "#F3E8FF", darkText: "#FFB9FF", darkBg: "#59168B" },
 };
--- front/entities/conference/schema.ts
+++ front/entities/conference/schema.ts
@@ -3,10 +3,18 @@ import { z } from "zod";
 export const conferenceFormSchema = z.object({
   title:              z.string().min(1, "Le titre est requis"),
   location:           z.string().min(1, "La localisation est requise"),
-  cfpLink:            z.string(), // required?
+  cfpLink:            z.string().optional(),
   cfpClosingDate:     z.string().optional(),
   cfpStatus:          z.enum(["Open", "Closed", "None"]),
   submittedTalksAmount: z.number(),
+  type: z.enum([
+    "Marketing / business",
+    "Technique stratégique",
+    "Technique généraliste",
+    "Technique",
+    "Hors scope"
+  ]),
+  reach: z.enum(["Locale", "Régionale", "Nationale"]),
 });
 
 export type ConferenceFormData = z.infer<typeof conferenceFormSchema>;
